ae6088097470d939849ebb34a6c23a513c0228f0
huldil <92667550+huldil@users.noreply.github.com>
2025-02-22 11:22:29 +0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Welcome to FreeImageGeneration</h1>
-      <p>AI-powered image generation platform.</p>
-    </div>
-  );
-}
